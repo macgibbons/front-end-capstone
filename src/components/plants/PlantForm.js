d5d8426b4a390e1e161e6291a1d0274a2887b64f
macgibbons <macgibbons@gmail.com>
2020-02-05 15:49:01 -0600

    user can now add a plant and edit a plant

--- src/components/plants/PlantForm.js
+++ src/components/plants/PlantForm.js
@@ -16,3 +16,208 @@ water amount - dropdown
 
 */
 
+import React, { useContext, useState, useEffect } from "react"
+import { PlantContext } from "./PlantProvider";
+import { RoomContext } from "../rooms/RoomProvider";
+
+export default props => {
+    const { rooms } = useContext(RoomContext)
+    const { addPlant, plants, updatePlant } = useContext(PlantContext)
+    const [plant, setPlant] = useState({})
+
+    
+    const editMode = props.match.params.hasOwnProperty("plantId")
+
+    const handleControlledInputChange = (event) => {
+        /*
+            When changing a state object or array, always create a new one
+            and change state instead of modifying current one
+        */
+        const newPlant = Object.assign({}, plant)
+        newPlant[event.target.name] = event.target.value
+        setPlant(newPlant)
+    }
+
+    const setDefaults = () => {
+        if (editMode) {
+            const plantId = parseInt(props.match.params.plantId)
+            const selectedPlant = plants.find(a => a.id === plantId) || {}
+            setPlant(selectedPlant)
+        }
+    }
+
+    useEffect(() => {
+        setDefaults()
+    }, [plants])
+
+    const constructNewPlant = () => {
+        const roomId = parseInt(plant.roomId)
+
+        if (roomId === 0) {
+            window.alert("Please select a room")
+        } else {
+            if (editMode) {
+                updatePlant({
+                    id: plant.id,
+                    name: plant.name,
+                    species: plant.species,
+                    lighting: plant.lighting,
+                    waterAmount: plant.waterAmount,
+                    waterFrequency: plant.waterFrequency,
+                    waterDay: plant.waterDay,
+                    userId: parseInt(localStorage.getItem("currentUser")),
+                    roomId: roomId,
+                    notes: plant.notes
+          
+                })
+                    .then(() => props.history.push("/plants"))
+            } else {
+                addPlant({
+                    id: plant.id,
+                    name: plant.name,
+                    species: plant.species,
+                    lighting: plant.lighting,
+                    waterAmount: plant.waterAmount,
+                    waterFrequency: plant.waterFrequency,
+                    waterDay: plant.waterDay,
+                    userId: parseInt(localStorage.getItem("currentUser")),
+                    roomId: roomId,
+                    notes: plant.notes
+                })
+                    .then(() => props.history.push("/plants"))
+            }
+        }
+    }
+
+    return (
+        <form className="plant--form container">
+            <h2 className="plant--formTitle">{editMode ? "Update Plant" : "New Plant"}</h2>
+            <div className="wrapper">
+                <fieldset>
+                    <div className="plant-form-group">
+                        <label htmlFor="name">Plant Name* </label>
+                        <input type="text" name="name" required autoFocus className="form-control"
+                            proptype="varchar"
+                            placeholder="plant name.."
+                            defaultValue={plant.name}
+                            onChange={handleControlledInputChange}
+                            />
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <div className="plant-form-group">
+                        <label htmlFor="species">Plant Species* </label>
+                        <input type="text" name="species" required className="form-control"
+                            proptype="varchar"
+                            placeholder="species..."
+                            defaultValue={plant.species}
+                            onChange={handleControlledInputChange}
+                            />
+                    </div>
+                </fieldset>
+            </div>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="roomId">room: </label>
+                    <select name="roomId" className="form-control"
+                        proptype="int"
+                        value={plant.roomId}
+                        onChange={handleControlledInputChange}>
+
+                        <option value="0">Select a room</option>
+                        {rooms.map(e => (
+                            <option key={e.id} value={e.id}>
+                                {e.roomName}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="lighting">lighting* </label>
+                    <select name="lighting" className="form-control"
+                        proptype="int"
+                        value={plant.lighting}
+                        onChange={handleControlledInputChange}>
+
+                        <option value="0">Select a lighting</option>
+                        <option value="1">low</option>
+                        <option value="2">medium</option>
+                        <option value="3">high</option>
+                        
+                    
+                    </select>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="waterDay">what day?* </label>
+                    <select name="waterDay" className="form-control"
+                        proptype="varchar"
+                        value={plant.waterDay}
+                        onChange={handleControlledInputChange}>
+
+                        <option value="0">Select a day</option>
+                        <option value="monday">monday</option>
+                        <option value="tuesday">tuesday</option>
+                        <option value="wednesday">wednesday</option>
+                        <option value="Thursday">thursday</option>
+                        <option value="Friday">friday</option>
+                        <option value="Saturday">saturday</option>
+                        <option value="Sunday">sunday</option>
+                        
+                        
+                    </select>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="waterAmount">how much water? </label>
+                    <input type="text" name="waterAmount" className="form-control"
+                        proptype="int"
+                        value={plant.waterAmount}
+                        onChange={handleControlledInputChange}/>
+                        cups
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="waterFrequency">Frequency* </label>
+                    <select name="waterFrequency" className="form-control"
+                        proptype="varchar"
+                        value={plant.waterFrequency}
+                        onChange={handleControlledInputChange}>
+
+                        <option value="0">how often?</option>
+                        <option value="1">once a week</option>
+                        <option value="2">once every other week</option>
+                        <option value="3">once a month</option>
+                    
+                        
+                        
+                    </select>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="notes">Notes: </label>
+                    <textarea type="text" name="notes" className="form-control"
+                        proptype="varchar"
+                        value={plant.notes}
+                        onChange={handleControlledInputChange}>
+                    </textarea>
+                </div>
+            </fieldset>
+            <button type="submit"
+                onClick={evt => {
+                    evt.preventDefault()
+                    constructNewPlant()
+                }}
+                className="btn btn-primary">
+                {editMode ? "Save Updates" : "Add Plant"}
+            </button>
+        </form>
+    )
+}
+
