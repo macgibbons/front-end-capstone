2651cc0f1b9a2818ee102830bb32c85ae016ea5d
macgibbons <macgibbons@gmail.com>
2020-02-05 11:41:31 -0600

    built out day provider and fixed userss typo on user provider

---
+++ src/components/days/DayProvider.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react"
+
+
+export const DayContext = React.createContext()
+
+/*
+ This component establishes what data can be used.
+ */
+export const DayProvider = (props) => {
+    // this is the name of the data in the entireity of the application!! (locations)
+    const [days, setDays] = useState([])
+
+    const getDays = () => {
+        return fetch("http://localhost:8088/days")
+            .then(res => res.json())
+            .then(setDays)
+    }
+
+
+
+    /*
+        Load all Days when the component is mounted. Ensure that
+        an empty array is the second argument to avoid infinite loop.
+    */
+
+    useEffect(() => {
+        getDays()
+    }, [])
+
+    useEffect(() => {
+        console.log("****  DAY APPLICATION STATE CHANGED  ****")
+    }, [days])
+
+    return (
+        <DayContext.Provider value={{
+            days
+        }}>
+            {props.children}
+        </DayContext.Provider>
+    )
+}
