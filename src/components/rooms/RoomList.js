9edaaf23058dcea7488bfd93302c7b6feb364a66
macgibbons <macgibbons@gmail.com>
2020-02-12 19:07:38 -0600

    recurrance is now functional completelly

--- src/components/rooms/RoomList.js
+++ src/components/rooms/RoomList.js
@@ -18,8 +18,9 @@ export default (props) => {
     const { plants } = useContext(PlantContext)
     const { days } = useContext(DayContext)
     const currentUser = parseInt(localStorage.getItem("currentUser"), 10)
-    const currentUserRooms = rooms.filter(r => r.userId == currentUser)
+    const currentUserRooms = rooms.filter(r => r.userId === currentUser)
 
+    
     const logInCheck = () => {
         if(currentUser === ""){
             window.alert("please log in")
@@ -32,16 +33,15 @@ export default (props) => {
     }
 
     const updateApplicationView = () => {
-        props.history.push("/plants/create")
+        props.history.push("/rooms/create")
     }
-    
 
     return (
         <div className="plant--container container">
             <div className="room--header">
                 <h1>My Rooms</h1>
                 <div className="btn add--btn"
-                    onClick={() => props.history.push("/rooms/create")}>
+                    onClick={() => {logInCheck()}}>
                     <img className="icon" src={require ('./add.svg')}/>
                 </div>
             </div>
--- src/components/today/TodayList.js
+++ src/components/today/TodayList.js
@@ -1,6 +1,4 @@
-// this is the component responsible for list ALL of the plants
 import React, { useContext, useState } from "react"
-// import Plant from "../plants/Plant"
 import { DayContext } from "../days/DayProvider";
 import { PlantContext } from "../plants/PlantProvider";
 import { UserContext } from "../users/UserProvider";
@@ -15,12 +13,11 @@ export default (props) => {
     const [ plantsToBeWatered, setPlantsToBeWatered ] = useState(0)
     const currentUser = parseInt(localStorage.getItem("currentUser"), 10)
     const currentUserName = users.find(u => u.id === currentUser) || {}
-    const currentUserPlants = plants.filter(p => p.userId == currentUser)
+    const currentUserPlants = plants.filter(p => p.userId === currentUser)
     var moment = require('moment')
 
     // function to find the long form of todays date
     const Today = new Date()
-    console.log(Today);
     
 
     // grabbing the number of the day of the week sunday = 0, monday = 1, etc. 
@@ -41,10 +38,8 @@ export default (props) => {
     todaysPlants.map(plant => {
         if(plant.waterFrequency === "once every other week" && moment(plant.lastWatered).add(14, 'days') <= moment(Date.now()) ){
             biweeklyPlants.push(plant) 
-            // console.log(plant.name + moment(plant.lastWatered).subtract(14, 'days'));
         }
     })
-        console.log(biweeklyPlants);
 
     //  finding all of the unwatered plants with a weekly schedule
     let weeklyPlants =[]
@@ -55,7 +50,6 @@ export default (props) => {
             weeklyPlants.push(plant) 
         }}
     })
-        console.log(weeklyPlants)
 
     //  finding all of the unwatered plants with a monthly schedule
     let monthlyPlants =[]
@@ -67,30 +61,54 @@ export default (props) => {
         }}
     })
 
-    console.log(monthlyPlants);
-    
+    // this combines all of the different plant arrays into one of plants that need to be watered today!
     const waterThesePlantsToday = weeklyPlants.concat(biweeklyPlants).concat(monthlyPlants)
 
-    console.log(waterThesePlantsToday);
+    // this is the patch function that will update a plant as not watered after a certain amount of days
+    // so that the user knows it's time to rewater it. 
+    const patchPlantsAsNotCompleted = (plant) => {
+        const updatePlantAsNotCompleted = {
+            id: plant.id,
+            isCompleted: false
+        };
+        patchPlant(updatePlantAsNotCompleted)
+    }
+
+    // here we are mapping over all of the user's plants and checking which ones are watered
+    // then we are seeing which ones are over due weekly, biweekly, and monthly 
+    currentUserPlants.map( plant =>{
+        if(plant.isCompleted === true){
+            if(plant.waterFrequency === "once a week"){
+                if(moment(plant.lastWatered).add(7, 'days') <= moment(Date.now()) ){
+               patchPlantsAsNotCompleted(plant)
+        }} else if (
+            plant.waterFrequency === "once every other week"){
+                if(moment(plant.lastWatered).add(14, 'days') <= moment(Date.now()) ) {
+                    patchPlantsAsNotCompleted(plant)
+        }} else if (
+            plant.waterFrequency === "once a month"){
+            if(moment(plant.lastWatered).add(28, 'days') <= moment(Date.now()) ) {
+                patchPlantsAsNotCompleted(plant)
+        }} 
+        }
+    })
 
 
-
+    // this function loops over the plants to water today and patches the isCompleted to true
+    // as well as updates the lastwatered date
     const markAllAsWatered = () => {
         waterThesePlantsToday.map(
             plant =>
-            
          {
             if(plant.isCompleted === false){
-                
-                
                 const updatePlantAsCompleted = {
                   id: plant.id,
                   lastWatered: Date.now(),
                   isCompleted: true
                 };
-                patchPlant(updatePlantAsCompleted)}
-            }
-        )
+            patchPlant(updatePlantAsCompleted)}
+        }
+      )
     }
 
     const markOtherDaysAsUnwatered = () => {
@@ -114,7 +132,10 @@ export default (props) => {
         <div className="plant--container">
             <h1 onChange={()=> {markOtherDaysAsUnwatered()}}>{nameOfToday}</h1>
             <h1>Welcome back {currentUserName.name}!</h1>
-            <h3>{watering ? (<h3>Congratulations! All of your plants are watered </h3>) : (<h3>You have {waterThesePlantsToday.length} plants to water today</h3>)}</h3>
+            <div>{watering ? 
+                (<h3>Congratulations! All of your plants are watered </h3>) :
+                (<h3>You have {waterThesePlantsToday.length} plants to water today</h3>)}
+            </div>
             <div className="checkbox">
                 <input type="checkbox" name="species"  className="form-control" onChange={()=>{markAllAsWatered()
                 markOtherDaysAsUnwatered()
